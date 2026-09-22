3554b0c1a29d22a71f6d1752f2b4219e733c8783
knox-night <sigmaboy789265@gmail.com>
2026-09-22 11:55:00 +0530

    Encrypt GitHub access tokens at rest

--- db.js
+++ db.js
@@ -1,5 +1,30 @@
+require("dotenv").config();
 const Database = require('better-sqlite3');
+
 const db = new Database('project-vs.db');
+const crypto = require("crypto");
+
+const ENCRYPTION_KEY = Buffer.from(process.env.TOKEN_ENCRYPTION_KEY, "hex");
+
+function encryptToken(token) {
+  const iv = crypto.randomBytes(12);
+  const cipher = crypto.createCipheriv("aes-256-gcm", ENCRYPTION_KEY, iv);
+  const encrypted = Buffer.concat([cipher.update(token, "utf8"), cipher.final()]);
+  const authTag = cipher.getAuthTag();
+  // Store iv, authTag, and ciphertext together, separated by colons
+  return iv.toString("hex") + ":" + authTag.toString("hex") + ":" + encrypted.toString("hex");
+}
+
+function decryptToken(encryptedToken) {
+  const [ivHex, authTagHex, dataHex] = encryptedToken.split(":");
+  const iv = Buffer.from(ivHex, "hex");
+  const authTag = Buffer.from(authTagHex, "hex");
+  const encrypted = Buffer.from(dataHex, "hex");
+  const decipher = crypto.createDecipheriv("aes-256-gcm", ENCRYPTION_KEY, iv);
+  decipher.setAuthTag(authTag);
+  const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
+  return decrypted.toString("utf8");
+}
 
 // Create users table if it doesn't already exist
 db.exec(`
@@ -40,13 +65,17 @@ function saveUser(githubUsername, accessToken) {
     ON CONFLICT(github_username)
     DO UPDATE SET access_token = excluded.access_token, updated_at = CURRENT_TIMESTAMP
   `);
-  stmt.run(githubUsername, accessToken);
+  stmt.run(githubUsername, encryptToken(accessToken));
 }
 
 // Get a user by their GitHub username
 function getUser(githubUsername) {
   const stmt = db.prepare('SELECT * FROM users WHERE github_username = ?');
-  return stmt.get(githubUsername);
+  const user = stmt.get(githubUsername);
+  if (user && user.access_token) {
+    user.access_token = decryptToken(user.access_token);
+  }
+  return user;
 }
 
 // Save/update a user's email address
@@ -59,7 +88,13 @@ function saveEmail(githubUsername, email) {
 // Get every user who has saved an email address
 function getAllUsers() {
   const stmt = db.prepare('SELECT * FROM users WHERE email IS NOT NULL');
-  return stmt.all();
+  const users = stmt.all();
+  return users.map(user => {
+    if (user.access_token) {
+      user.access_token = decryptToken(user.access_token);
+    }
+    return user;
+  });
 }
 // Mute a notification thread for a user
 function muteNotification(githubUsername, notificationId) {
